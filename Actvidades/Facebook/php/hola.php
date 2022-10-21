40fb8b08f7cf28a9f95a33e603219f7e32843f74
Narbian <114744752+Leonardo12433@users.noreply.github.com>
2022-10-21 07:23:15 -0500

    21/10/22

---
+++ Actvidades/Facebook/php/hola.php
@@ -0,0 +1,9 @@
+<?php
+$nombre=$_GET['nam'];
+$contraseña=$_GET['pass'];
+$email=$_GET['em'];
+
+echo "Bievenido ala pagina $nam";
+echo "<br>";
+echo "Tu contraseña y email ya esta guardadas $pass, $em";
+?>
